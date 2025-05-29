869a8b0996e6a1b49a30289b0e38d6e8eab83e3b
JustinDubois77 <jdubois@stratj.ca>
2025-05-29 10:59:04 -0400

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+import authentification from './authentification'
+import users from './users'
+
+const router = express.Router();
+
+export default (): express.Router => {
+    authentification(router);
+    users(router);
+
+    return router;
+}
