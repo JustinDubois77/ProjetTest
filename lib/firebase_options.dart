982c71bde7968d1c369899a87c61bc53f3529a91
JustinDubois77 <jdubois@stratj.ca>
2025-05-30 13:39:06 -0400

    Tentative de faire fonctionner firebase (onfiguration-not-found)

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,62 +1,20 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
-/// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
-/// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      return web;
-    }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for android - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.iOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      default:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions are not supported for this platform.',
-        );
-    }
+    if (kIsWeb) return web;
+    throw UnsupportedError(
+      'DefaultFirebaseOptions are only configured for Web. '
+      'Reconfigure using FlutterFire CLI to support other platforms.',
+    );
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-      apiKey: "AIzaSyC_egpnH_cZQiOAZRDLBzV-uxAOXRLNm_U",
-      authDomain: "projet-test2-3b2e0.firebaseapp.com",
-      databaseURL: "https://projet-test2-3b2e0-default-rtdb.firebaseio.com",
-      projectId: "projet-test2-3b2e0",
-      storageBucket: "projet-test2-3b2e0.firebasestorage.app",
-      messagingSenderId: "698229985917",
-      appId: "1:698229985917:web:ad58de38f3b930a460d714",
-      measurementId: "G-N3CVYN94PE");
+      apiKey: "AIzaSyATwiaLtXdeT7ppYEbj-8T0MRSdvDKwfzQ",
+      authDomain: "testfirebase-30c7f.firebaseapp.com",
+      projectId: "testfirebase-30c7f",
+      storageBucket: "testfirebase-30c7f.firebasestorage.app",
+      messagingSenderId: "10299087558",
+      appId: "1:10299087558:web:86b0bcdb31dacd58d7d5c7");
 }
